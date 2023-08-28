5f1fc0d0995c0151b1764f91845e4b7732c45a2c
gregzakh <grishanz@yandex.com>
2023-08-28 23:31:08 +0500

    A  windbg/clockres.js

---
+++ windbg/clockres.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const log = x => host.diagnostics.debugLog(`${x}\n`);
+/*
+ * Usage: .scriptload clockres.js
+ *        !clockres
+ */
+function *GetSystemTimerResolution() {
+  if (!host.currentSession.Attributes.Target.IsKernelTarget) {
+    log('Error: requires kernel mode debugger environment.');
+    return;
+  }
+
+  ['Maximum', 'Minimum', 'Current'].forEach(x => {
+    let value = host.createPointerObject(
+      host.getModuleSymbolAddress(
+        'nt', `Ke${"Current" === x ? "Time" : x}Increment`
+      ), 'nt', 'unsigned long *'
+    )[0];
+    log(`${x} timer interval: ${parseFloat(value / 10000)} ms`);
+  });
+}
+
+function initializeScript() {
+  return [new host.functionAlias(GetSystemTimerResolution, 'clockres')];
+}
