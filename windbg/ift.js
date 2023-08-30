6fa7ef8bb3e4dbb020baad4cbaac19dbaff13b7b
gregzakh <grishanz@yandex.com>
2023-08-30 22:45:45 +0500

    A  windbg/ift.js

---
+++ windbg/ift.js
@@ -0,0 +1,33 @@
+'use strict';
+/*
+ * Usage: .scriptload ift.js
+ *        dx -g @$ift()
+ *        or
+ *        dx -g @$ift("Ps")
+ *
+ * Getting the same result with manual input:
+ * dx @$table = *(nt!_INVERTED_FUNCTION_TABLE **)&nt!KeUserInvertedFunctionTable
+ * dx -g @$table->TableEntry->Take(@$table->CurrentSize)
+ * ; PsInvertedFunctionTable
+ * dx -g ((nt!_INVERTED_FUNCTION_TABLE *)&nt!PsInvertedFunctionTable)->TableEntry->Take(0xBE)
+ */
+function *GetInvertedFunctionTable(prefix = 'KeUser') {
+  if (!host.currentSession.Attributes.Target.IsKernelTarget) {
+    host.diagnostics.debugLog('Error: invalid debugger environment.\n');
+    return;
+  }
+
+  let table = host.createPointerObject(
+    host.getModuleSymbolAddress('nt', `${prefix}InvertedFunctionTable`),
+    'nt', `_INVERTED_FUNCTION_TABLE ${'Ps' === prefix ? '*' : '**'}`
+  );
+
+  if ('KeUser' === prefix) table = table.dereference();
+  for (let entry of table.TableEntry.Take(
+    'KeUser' === prefix ? table.CurrentSize : 0xBE
+  )) yield entry;
+}
+
+function initializeScript() {
+  return [new host.functionAlias(GetInvertedFunctionTable, 'ift')];
+}
